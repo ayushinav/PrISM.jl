42b4173a6604796c2a2658744fe3d5c9aa593904
Abhinav Pratap Singh <abhinav.ism.iit@gmail.com>
2025-12-23 20:49:09 -0500

    feat : DC model added

--- src/models/dc/forward.jl
+++ src/models/dc/forward.jl
@@ -13,62 +13,14 @@ function get_T(m, h, k)
     return T
 end
 
-"""
-r_max : maximum r
-r_min : minimum r
-fn : kernel function
-hankel_filter : filter to be used, contains base and J₀
-
-returns an interpolated function that outputs the integral using Hankel transform at any r
-"""
-function do_Hankel_stuff(r_max, r_min, fn, hankel_filter)
-    base_ = hankel_filter.base
-    j0 = hankel_filter.J₀
-    n_filter = length(base_)
-    f = log(base_[2]) - log(base_[1])
-
-    n_r = Int(ceil((log(r_max) - log(r_min)) / f)) + 1
-    log_r_end = log(r_min) + (n_r - 1) * f # a + (n-1)d on log-scale
-
-    rs = exp.(range(log_r_end, log(r_min); length=n_r))
-    nλ = n_filter + n_r - 1
-
-    log_λ_max = log(base_[end] / r_min)
-    log_λ_start = log_λ_max - (nλ - 1) * f
-    λs = exp.(range(log_λ_start, log_λ_max; length=nλ))
-
-    T_at_λs = fn.(λs)
-    T_at_rs = zeros(n_r)
-
-    for ir in 1:n_r
-        T_at_rs[ir] = inv(rs[ir]) .* (j0 ⋅ T_at_λs[ir:(ir + n_filter - 1)])
-    end
-
-    # make interpolating function and return
-
-    f_spline = cubic_spline_interpolation(
-        range(log(r_min), log_r_end; length=n_r), reverse(T_at_rs))
-
-    return f_spline
-end
-"""
-we can have relative r_locs
-src_a_loc : 1 element
-src_b_loc : 1 element
-rec_m_loc : n, element
-rec_n_loc : n, element
-
-srcs = [src_a_loc, src_b_loc]
-recs = hcat(rec_m_loc ; rec_n_loc) n x 2
-"""
-function fwd(m, h, srcs, recs, hankel_filter)
+function SubsurfaceCore.forward(m::Tm, locs::T3, params) where {Tm <: DCModel, T3}
     r_a = abs.(recs[:] .- srcs[1])
     r_b = abs.(recs[:] .- srcs[2])
     r_min, r_max = extrema([r_a..., r_b...])
 
     fn(k) = get_T(m, h, k)
 
-    V_fn = do_Hankel_stuff(r_max, r_min, fn, hankel_filter)
+    V_fn = hankel_transform_and_interpolation(r_max, r_min, fn, params.hankel_filter)
 
     r_am = abs.(recs[:, 1] .- srcs[1])
     V_am = V_fn.(log.(r_am))
---
+++ src/models/dc/types.jl
@@ -0,0 +1,43 @@
+mutable struct DCResponse{T1, T2} <: AbstractGeophyResponse
+    ρₐ::T1
+    ϕ::T2
+end
+
+#=
+More often than not, T1 and T2 will be same, that is typeof(ρₐ) and typeof(ϕ) will be same but this might become different when using a rock physics model in front of this. 
+For now, we go with the same design as for `DCModel`.
+=#
+
+"""
+create a model type for a given resistivity distribution
+that can be used to calculate forward response for 1d DC
+
+## Usage
+
+```jldoctest
+m = [4.0, 2.0, 3.0]
+h = [1000.0, 1000.0]
+model = DCModel(m, h)
+print(model)
+
+# output
+
+1D DCModel : 
+Layer    h       log(ρ)
+____________________________
+1        1000.0          4.0
+2        1000.0          2.0
+3        ∞               3.0
+```
+"""
+mutable struct DCModel{T1 <: AbstractArray{<:Any}, T2 <: AbstractArray{<:Any}} <:
+               AbstractGeophyModel{T1, T2}
+    m::T1
+    h::T2
+end
+
+# explain in blog the reasoning behind this! This covers all 1D, 2D, 3D models for DC.
+# make pretty tables to print these models
+
+SubsurfaceCore.default_params(::Type{T}) where {T <: AbstractGeophyModel} = (; hankel_filter = Key101)
+const default_params_DC = default_params(DCModel)
