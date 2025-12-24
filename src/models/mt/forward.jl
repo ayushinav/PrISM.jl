088f0e8083bf8b1afe3c6f6a85cf47b1414b15ec
Abhinav Pratap Singh <abhinav.ism.iit@gmail.com>
2025-12-23 20:49:47 -0500

    chore : restructure MT forward codes

--- src/models/mt/forward.jl
+++ src/models/mt/forward.jl
@@ -8,23 +8,6 @@ returns a tuple of ρₐ and ϕ, given arrays of resistivity `ρ` and thickness 
 
 const default_mt_tf_fns = (ρₐ=no_tf, ϕ=no_tf)
 
-function get_Z(ρ::T1, h::T2, ω::T) where {T1, T2, T}
-    broadcast!(exp10, ρ, ρ)
-    k = sqrt(im * ω * μ / ρ[end])
-    Z = ω * μ / k
-
-    j = length(h)
-    @inbounds while j >= 1
-        k = sqrt(im * ω * μ / ρ[j])
-        Z = ω * μ / k * coth(-im * k * h[j] + acoth(Z / (ω * μ / k)))
-        j -= 1
-    end
-
-    broadcast!(log10, ρ, ρ)
-    Z = conj(Z)
-    return get_appres(Z, ω), get_phase(Z)
-end
-
 # dispatch on forward for 1d model
 """
 `forward(m::model, ω::Vector{T}) where T <: Union{Float32, Float64}`:
@@ -76,15 +59,3 @@ function forward!(r::Tr, m::Tm, ω::T3, response_trans_utils::T=default_mt_tf_fn
     broadcast!(f2, r.ϕ, r.ϕ)
     nothing
 end
-
-# the following are defined on scalars, there in-place variants don't make sense
-
-"""
-`get_phase(Z)`: returns the phase for impedance
-"""
-get_phase(Z) = 180 / π * atan(imag(Z) / real(Z));
-
-"""
-`get_appres(Z, ω)`: returns the ρₐ for impedance
-"""
-get_appres(Z, ω) = abs(Z)^2 / (eltype(ω))(μ) / ω;
