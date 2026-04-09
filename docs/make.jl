ce8b1ec1f22c35103bcb54f07f4956734a3a8cb5
Abhinav Pratap Singh <87991961+ayushinav@users.noreply.github.com>
2026-04-09 16:23:46 -0400

    docs : fixes so docs actually deploy

--- docs/make.jl
+++ docs/make.jl
@@ -7,14 +7,14 @@ DocMeta.setdocmeta!(PrISM, :DocTestSetup, :(using PrISM); recursive=true)
 makedocs(; authors="Abhinav Pratap Singh et al.",
     sitename="PrISM.jl",
     doctest=false,
-    # clean=true,
+    clean=true,
     repo="github.com/ayushinav/PrISM.jl",
     format=DocumenterVitepress.MarkdownVitepress(;
         repo="github.com/ayushinav/PrISM.jl", devurl="dev", devbranch="main"),
     pages=pages,
-    # draft=false,
-    # source="src",
+    draft=false,
+    source="src",
     build="build")
 
-# DocumenterVitepress.deploydocs(; repo="github.com/ayushinav/PrISM.jl",
-#     target=joinpath(@__DIR__, "build"), devbranch="main", push_preview=true)
+DocumenterVitepress.deploydocs(; repo="github.com/ayushinav/PrISM.jl",
+    target=joinpath(@__DIR__, "build"), devbranch="main", push_preview=true)
