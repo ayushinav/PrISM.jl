8a522f66684c4729e2f8f8a92280cf3e9c8d79a6
Abhinav Pratap Singh <abhinav.ism.iit@gmail.com>
2026-05-11 21:49:12 -0400

    feat : custom rule for forward mode enzyme for surface waves using finite diff

--- ext/PrISMEnzymeExt.jl
+++ ext/PrISMEnzymeExt.jl
@@ -6,72 +6,254 @@ using .EnzymeRules
 import PrISM: find_c_
 
 function _ift_find_c_enzyme(f_func, c, ω_val, m_val, m_dval)
-    fₓ = first(Enzyme.autodiff(Enzyme.Forward, c_ -> f_func(c_, ω_val, m_val),
-                               Enzyme.Duplicated(c, one(c))))
-    fₚ = first(Enzyme.autodiff(Enzyme.Forward, m_ -> f_func(c, ω_val, m_),
-                               Enzyme.Duplicated(m_val, m_dval)))
+    @show typeof(c), typeof(ω_val), typeof(m_val), typeof(m_dval)
+    # @show "rev"
+
+    # c1 = one(c)
+    # f1(c_) = f_func(c_, ω_val, m_val)
+    # # fₓ = first(Enzyme.autodiff(Enzyme.Forward, Const(f1),
+    # #                            Enzyme.Duplicated(c, c1)))
+    # fₓ = DifferentiationInterface.derivative(f1, AutoEnzyme(; mode = Reverse), c)
+    # # fₚ = first(Enzyme.autodiff(Enzyme.Forward, m_ -> f_func(c, ω_val, m_),
+    # #                            Enzyme.Duplicated(m_val, m_dval)))
+    # # return -fₚ / fₓ
+    # return fₓ
+
+    ε = sqrt(eps(c))
+    f1 = f_func(c + ε, ω_val, m_val)
+    f2 = f_func(c - ε, ω_val, m_val)
+    fₓ = (f1 - f2) / (2ε)
+    m_pert1 = deepcopy(m_val)
+    m_pert2 = deepcopy(m_val)
+
+    m_pert1.m .= m_val.m .+ ε .* _dval.m
+    m_pert1.h .= m_val.h .+ ε .* _dval.h
+    m_pert1.ρ .= m_val.ρ .+ ε .* m_dval.ρ
+    m_pert1.vp .= m_val.vp .+ ε .* m_dval.vp
+
+    m_pert2.m .= m_val.m .- ε .* _dval.m
+    m_pert2.h .= m_val.h .- ε .* m_dval.h
+    m_pert2.ρ .= m_val.ρ .- ε .* m_dval.ρ
+    m_pert2.vp .= m_val.vp .- ε .* m_dval.vp
+
+    fₚ = (f_func(c, ω_val, m_pert1) - f_func(c, ω_val, m_pert2)) / (2ε)
+    -fₚ / fₓ
+    # return 1.
+end
+
+function _ift_find_c_enzyme2(f_func, c, ω_val, m)
+    @show typeof(c), typeof(ω_val), typeof(m)
+
+    c1 = one(c)
+    f1(c_) = f_func(c_, ω_val, m.val)
+    fₓ = first(Enzyme.autodiff(Enzyme.Forward, Const(f1), Enzyme.Duplicated(c, c1)))
+    fₚ = first(Enzyme.autodiff(Enzyme.Forward, m_ -> f_func(c, ω_val, m_), m))
     return -fₚ / fₓ
+    # return fₓ
+    # return 1.
 end
 
 # RWModel
 
-function EnzymeRules.forward(
-        ::FwdConfigWidth, ::Const{typeof(find_c_)}, 
-        ::Type{<:Duplicated}, f::Const,
-        c1::T1, c2::T2, ω::Enzyme.Const, m::Duplicated{<:RWModel}) where {T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation}
-    println("Using custom rule!")
+function EnzymeRules.forward(::FwdConfigWidth, ::Const{typeof(find_c_)},
+        ::Type{<:Duplicated}, f::Const, c1::T1, c2::T2, ω::Enzyme.Const,
+        m::Duplicated{<:RWModel}) where {T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation}
+    println("Using custom rule! : Duplicated")
 
-    c  = find_c_(f.val, c1.val, c2.val, ω.val, m.val)
+    c = find_c_(f.val, c1.val, c2.val, ω.val, m.val)
     dc = _ift_find_c_enzyme(f.val, c, ω.val, m.val, m.dval)
+    # dc = _ift_find_c_enzyme2(f.val, c, ω.val, m)
 
     return Duplicated(c, dc)
 end
 
-function EnzymeRules.forward(
-        ::FwdConfigWidth, ::Const{typeof(find_c_)}, 
-        ::Type{<:DuplicatedNoNeed}, f::Const,
-        c1::T1, c2::T2, ω::Enzyme.Const, m::Duplicated{<:RWModel}) where {T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation}
-    println("Using custom rule!")
+function EnzymeRules.forward(::FwdConfigWidth, ::Const{typeof(find_c_)},
+        ::Type{<:DuplicatedNoNeed}, f::Const, c1::T1, c2::T2, ω::Enzyme.Const,
+        m::Duplicated{<:RWModel}) where {T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation}
+    println("Using custom rule! : DuplicatedNoNeed")
 
     c = find_c_(f.val, c1.val, c2.val, ω.val, m.val)
     dc = _ift_find_c_enzyme(f.val, c, ω.val, m.val, m.dval)
+    # dc = _ift_find_c_enzyme2(f.val, c, ω.val, m.val)
 
     return dc
 end
 
-
 # batch: fₓ computed once, fₚ per direction
-function _ift_find_c_enzyme(f_func, c, ω_val, m_val, m_dvals::NTuple{N}) where {N}
-    fₓ = only(Enzyme.autodiff(Enzyme.Forward, c_ -> f_func(c_, ω_val, m_val),
-                               Enzyme.Duplicated(c, one(c))))
-    return ntuple(N) do i
-        fₚ = only(Enzyme.autodiff(Enzyme.Forward, m_ -> f_func(c, ω_val, m_),
-                                   Enzyme.Duplicated(m_val, m_dvals[i])))
+function _ift_find_c_enzyme_batch(f_func, c, ω_val, m_val, m_dvals::NTuple{N}) where {N}
+    ε = sqrt(eps(c))
+    f1 = f_func(c + ε, ω_val, m_val)
+    f2 = f_func(c - ε, ω_val, m_val)
+    fₓ = (f1 - f2) / (2ε)
+    m_pert1 = deepcopy(m_val)
+    m_pert2 = deepcopy(m_val)
+    return ntuple(Val(N)) do i
+        # fₚ = first(Enzyme.autodiff(Enzyme.Forward, m_ -> f_func(c, ω_val, m_),
+        #                            Enzyme.Duplicated(m_val, m_dvals[i])))
         # -fₚ / fₓ
-        fₓ
+        # fp1 = f_fun()
+
+        md = m_dvals[i]
+        m_pert1.m .= m_val.m .+ ε .* md.m
+        m_pert1.h .= m_val.h .+ ε .* md.h
+        m_pert1.ρ .= m_val.ρ .+ ε .* md.ρ
+        m_pert1.vp .= m_val.vp .+ ε .* md.vp
+
+        m_pert2.m .= m_val.m .- ε .* md.m
+        m_pert2.h .= m_val.h .- ε .* md.h
+        m_pert2.ρ .= m_val.ρ .- ε .* md.ρ
+        m_pert2.vp .= m_val.vp .- ε .* md.vp
+
+        fₚ = (f_func(c, ω_val, m_pert1) - f_func(c, ω_val, m_pert2)) / (2ε)
+        -fₚ / fₓ
     end
 end
 
-function EnzymeRules.forward(
-        ::FwdConfigWidth, ::Const{typeof(find_c_)},
-        ::Type{<:BatchDuplicated}, f::Const,
-        c1::T1, c2::T2, ω::Enzyme.Const,
-        m::BatchDuplicated{<:RWModel, N}) where {T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation, N}
-    println("Using custom rule!")
-    c   = find_c_(f.val, c1.val, c2.val, ω.val, m.val)
-    dcs = _ift_find_c_enzyme(f.val, c, ω.val, m.val, m.dval)
+function EnzymeRules.forward(::FwdConfigWidth,
+        ::Const{typeof(find_c_)},
+        ::Type{<:BatchDuplicated},
+        f::Const,
+        c1::T1,
+        c2::T2,
+        ω::Enzyme.Const,
+        m::BatchDuplicated{<:RWModel, N}) where {
+        T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation, N}
+    # println("Using custom rule! : BatchDuplicated 2")
+    c = find_c_(f.val, c1.val, c2.val, ω.val, m.val)
+    dcs = _ift_find_c_enzyme_batch(f.val, c, ω.val, m.val, m.dval)
     return BatchDuplicated(c, dcs)
 end
 
+function EnzymeRules.forward(::FwdConfigWidth,
+        ::Const{typeof(find_c_)},
+        ::Type{<:BatchDuplicatedNoNeed},
+        f::Const,
+        c1::T1,
+        c2::T2,
+        ω::Enzyme.Const,
+        m::BatchDuplicated{<:RWModel, N}) where {
+        T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation, N}
+    println("Using custom rule! : BatchDuplicatedNoNeed")
+    c = find_c_(f.val, c1.val, c2.val, ω.val, m.val)
+    dcs = _ift_find_c_enzyme_batch(f.val, c, ω.val, m.val, m.dval)
+    return dcs
+end
 
-function EnzymeRules.forward(
-        ::FwdConfigWidth, ::Const{typeof(find_c_)},
-        ::Type{<:BatchDuplicatedNoNeed}, f::Const,
-        c1::T1, c2::T2, ω::Enzyme.Const,
-        m::BatchDuplicated{<:RWModel, N}) where {T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation, N}
-    println("Using custom rule!")
-    c   = find_c_(f.val, c1.val, c2.val, ω.val, m.val)
-    dcs = _ift_find_c_enzyme(f.val, c, ω.val, m.val, m.dval)
+function _ift_find_c_enzyme(f_func, c, ω_val, m_val::LWModel, m_dval)
+    # @show typeof(c), typeof(ω_val), typeof(m_val), typeof(m_dval)
+    # @show "rev"
+
+    # c1 = one(c)
+    # f1(c_) = f_func(c_, ω_val, m_val)
+    # # fₓ = first(Enzyme.autodiff(Enzyme.Forward, Const(f1),
+    # #                            Enzyme.Duplicated(c, c1)))
+    # fₓ = DifferentiationInterface.derivative(f1, AutoEnzyme(; mode = Reverse), c)
+    # # fₚ = first(Enzyme.autodiff(Enzyme.Forward, m_ -> f_func(c, ω_val, m_),
+    # #                            Enzyme.Duplicated(m_val, m_dval)))
+    # # return -fₚ / fₓ
+    # return fₓ
+
+    ε = sqrt(eps(c))
+    f1 = f_func(c + ε, ω_val, m_val)
+    f2 = f_func(c - ε, ω_val, m_val)
+    fₓ = (f1 - f2) / (2ε)
+    m_pert1 = deepcopy(m_val)
+    m_pert2 = deepcopy(m_val)
+
+    m_pert1.m .= m_val.m .+ ε .* _dval.m
+    m_pert1.h .= m_val.h .+ ε .* _dval.h
+    m_pert1.ρ .= m_val.ρ .+ ε .* m_dval.ρ
+
+    m_pert2.m .= m_val.m .- ε .* _dval.m
+    m_pert2.h .= m_val.h .- ε .* m_dval.h
+    m_pert2.ρ .= m_val.ρ .- ε .* m_dval.ρ
+
+    fₚ = (f_func(c, ω_val, m_pert1) - f_func(c, ω_val, m_pert2)) / (2ε)
+    -fₚ / fₓ
+    # return 1.
+end
+
+#LWModel
+
+function EnzymeRules.forward(::FwdConfigWidth, ::Const{typeof(find_c_)},
+        ::Type{<:Duplicated}, f::Const, c1::T1, c2::T2, ω::Enzyme.Const,
+        m::Duplicated{<:LWModel}) where {T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation}
+    # println("Using custom rule! : Duplicated")
+
+    c = find_c_(f.val, c1.val, c2.val, ω.val, m.val)
+    dc = _ift_find_c_enzyme(f.val, c, ω.val, m.val, m.dval)
+    # dc = _ift_find_c_enzyme2(f.val, c, ω.val, m)
+
+    return Duplicated(c, dc)
+end
+
+function EnzymeRules.forward(::FwdConfigWidth, ::Const{typeof(find_c_)},
+        ::Type{<:DuplicatedNoNeed}, f::Const, c1::T1, c2::T2, ω::Enzyme.Const,
+        m::Duplicated{<:LWModel}) where {T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation}
+    # println("Using custom rule! : DuplicatedNoNeed")
+
+    c = find_c_(f.val, c1.val, c2.val, ω.val, m.val)
+    dc = _ift_find_c_enzyme(f.val, c, ω.val, m.val, m.dval)
+    # dc = _ift_find_c_enzyme2(f.val, c, ω.val, m.val)
+
+    return dc
+end
+
+# batch: fₓ computed once, fₚ per direction
+function _ift_find_c_enzyme_batch(
+        f_func, c, ω_val, m_val::LWModel, m_dvals::NTuple{N}) where {N}
+    ε = sqrt(eps(c))
+    f1 = f_func(c + ε, ω_val, m_val)
+    f2 = f_func(c - ε, ω_val, m_val)
+    fₓ = (f1 - f2) / (2ε)
+    m_pert1 = deepcopy(m_val)
+    m_pert2 = deepcopy(m_val)
+    return ntuple(Val(N)) do i
+        # fₚ = first(Enzyme.autodiff(Enzyme.Forward, m_ -> f_func(c, ω_val, m_),
+        #                            Enzyme.Duplicated(m_val, m_dvals[i])))
+        # -fₚ / fₓ
+        # fp1 = f_fun()
+
+        md = m_dvals[i]
+        m_pert1.m .= m_val.m .+ ε .* md.m
+        m_pert1.h .= m_val.h .+ ε .* md.h
+        m_pert1.ρ .= m_val.ρ .+ ε .* md.ρ
+
+        m_pert2.m .= m_val.m .- ε .* md.m
+        m_pert2.h .= m_val.h .- ε .* md.h
+        m_pert2.ρ .= m_val.ρ .- ε .* md.ρ
+
+        fₚ = (f_func(c, ω_val, m_pert1) - f_func(c, ω_val, m_pert2)) / (2ε)
+        -fₚ / fₓ
+    end
+end
+
+function EnzymeRules.forward(::FwdConfigWidth,
+        ::Const{typeof(find_c_)},
+        ::Type{<:BatchDuplicated},
+        f::Const,
+        c1::T1,
+        c2::T2,
+        ω::Enzyme.Const,
+        m::BatchDuplicated{<:LWModel, N}) where {
+        T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation, N}
+    # println("Using custom rule! : BatchDuplicated 2")
+    c = find_c_(f.val, c1.val, c2.val, ω.val, m.val)
+    dcs = _ift_find_c_enzyme_batch(f.val, c, ω.val, m.val, m.dval)
+    return BatchDuplicated(c, dcs)
+end
+
+function EnzymeRules.forward(::FwdConfigWidth,
+        ::Const{typeof(find_c_)},
+        ::Type{<:BatchDuplicatedNoNeed},
+        f::Const,
+        c1::T1,
+        c2::T2,
+        ω::Enzyme.Const,
+        m::BatchDuplicated{<:LWModel, N}) where {
+        T1 <: Enzyme.Annotation, T2 <: Enzyme.Annotation, N}
+    c = find_c_(f.val, c1.val, c2.val, ω.val, m.val)
+    dcs = _ift_find_c_enzyme_batch(f.val, c, ω.val, m.val, m.dval)
     return dcs
 end
 
