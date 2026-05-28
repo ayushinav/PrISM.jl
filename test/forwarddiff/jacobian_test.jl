9a8c7e4629ff604e6a40f3b53c6bc63d255eee36
Abhinav Pratap Singh <abhinav.ism.iit@gmail.com>
2026-05-28 18:46:35 -0400

    chore : test on bigger models

--- test/forwarddiff/jacobian_test.jl
+++ test/forwarddiff/jacobian_test.jl
@@ -5,11 +5,12 @@ Cache_DI = DifferentiationInterface.Cache
 Constant_DI = DifferentiationInterface.Constant
 
 model_types = [MTModel, RWModel, LWModel]
-true_models = [(; m=[2.0, 1.0, 2.0], h=[1000.0, 1000.0]),
-    (; m=[4000.0, 3500.0, 4000.0] ./ 1e3, h=[1000.0, 1000.0],
-        vp=[7000.0, 7000.0, 7000.0] ./ 1e3, ρ=[2500.0, 2500.0, 2500.0] ./ 1e3),
-    (; m=[3500.0, 3600.0, 3800.0] ./ 1e3, h=[1000.0, 1000.0],
-        ρ=[2500.0, 2500.0, 2500.0] ./ 1e3)]
+
+true_models = ((; m=randn(50) .* 1.0 .+ 2, h=fill(100.0, 49)),
+    (; m=rand(50) .* 20e-1 .+ 3.0, h=fill(100.0, 49), vp=fill(7.5, 50), ρ=fill(2.5, 50)),
+    (; m=rand(50) .* 20e-1 .+ 3, h=fill(100.0, 49), ρ=fill(2.5, 50)),
+    (; m=randn(50) .* 0.01 .+ 2, h=fill(100.0, 49)))
+
 vars = [10.0 .^ collect(-3:0.1:1), 10.0 .^ collect(0:0.1:3), 10.0 .^ collect(-1:0.1:1)]
 
 adtype_baseline = AutoFiniteDiff()
